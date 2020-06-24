0cafa7fc4fae76c06ca0b5c1be0a73f39ab4d881
Vineet Parvathala <vparv@umich.edu>
2020-06-24 13:15:27 -0400

    add desc typed

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,8 @@
-var name = "Hi, I'm Vineet! I'm a junior at the University of Michigan - Ann Arbor, studying Computer Science. 🚀"
-var i = 0;
+
 
 function typeName() {
+var name = "Hi, I'm Vineet! I'm a junior at the University of Michigan Ann Arbor, studying Computer Science. 🚀"
+var i = 0;
 if (i < name.length) {
   var nameHeader = document.getElementById("nametag");
   console.log(nameHeader);
@@ -10,9 +11,26 @@ if (i < name.length) {
   setTimeout(typeName, 50);
 } 
 else {
-  i = 0;
-//setTimeout(typeName, 100);
+  typeDesc();
+
 }
 }
 
+function typeDesc() {
+    name = "I'm from Princeton, New Jersey and when I have free time, I enjoy coding 💻, learning new things 💡, and checking out new restaurants 🍽️."
+    i = 0;
+    if (i < name.length) {
+      nameHeader = document.getElementById("desc");
+      console.log(nameHeader);
+      nameHeader.innerHTML = nameHeader.innerHTML + name.charAt(i);
+      i += 1;
+      setTimeout(typeName, 50);
+    } 
+    else {
+      i = 0;
+    //setTimeout(typeName, 100);
+    }
+    }
+
 window.onload = typeName;
+
