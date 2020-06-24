05b3112e03b683d635454bbff353489839fc8d25
Vineet Parvathala <vparv@umich.edu>
2020-06-24 13:20:10 -0400

    added full typing

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,9 @@
 
+var i = 0;
 
 function typeName() {
 var name = "Hi, I'm Vineet! I'm a junior at the University of Michigan Ann Arbor, studying Computer Science. 🚀"
-var i = 0;
+
 if (i < name.length) {
   var nameHeader = document.getElementById("nametag");
   console.log(nameHeader);
@@ -11,6 +12,7 @@ if (i < name.length) {
   setTimeout(typeName, 50);
 } 
 else {
+    i = 0;
   typeDesc();
 
 }
@@ -18,7 +20,23 @@ else {
 
 function typeDesc() {
     name = "I'm from Princeton, New Jersey and when I have free time, I enjoy coding 💻, learning new things 💡, and checking out new restaurants 🍽️."
-    i = 0;
+    
+    if (i < name.length) {
+      nameHeader = document.getElementById("desc");
+      console.log(nameHeader);
+      nameHeader.innerHTML = nameHeader.innerHTML + name.charAt(i);
+      i += 1;
+      setTimeout(typeName, 50);
+    } 
+    else {
+      i = 0;
+    typeThanks();
+    }
+}
+
+function typeThanks() {
+    name = "Thanks for checking out my website ❤️ "
+    
     if (i < name.length) {
       nameHeader = document.getElementById("desc");
       console.log(nameHeader);
@@ -30,7 +48,7 @@ function typeDesc() {
       i = 0;
     //setTimeout(typeName, 100);
     }
-    }
+}
 
 window.onload = typeName;
 
